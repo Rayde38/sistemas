9737c7f2b1de3250a0c6423fc6b2f77d86825357
Rayde38 <retinferel@gmail.com>
2025-08-27 10:58:53 -0400

    Initial commit - Laravel project ready for Railway deployment

---
+++ app/Models/Categoria.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'identificador',
+        'fecha_creacion',
+        'fecha_modificacion'
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
+    public function getNombreAttribute($value)
+    {
+        return ucfirst($value);
+    }
+}
